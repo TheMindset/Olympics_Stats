fced4bc57ea3fa144007e109a849d86665fa450d
TheMindset <mulezig2014@outlook.fr>
2019-12-23 20:28:47 +0100

    Add some logic for handle Youngest/Oldest requests

--- app/controllers/api/v1/competitors_controller.rb
+++ app/controllers/api/v1/competitors_controller.rb
@@ -4,13 +4,20 @@ module Api
   module V1
     class CompetitorsController < ApplicationController
       def index
-        render json: CompetitorSerializer.new(
-          Competitor.includes(
-            :team,
-            :events,
-            :sports
-          )
-        )
+        competitor_data = if params[:age]
+                            if params[:age] == 'youngest'
+                              Competitor.youngest_competitor
+                            else
+                              Competitor.oldest_competitor
+                            end
+                          else
+                            Competitor.includes(
+                              :team,
+                              :events,
+                              :sports
+                            )
+                          end
+        render json: CompetitorSerializer.new(competitor_data)
       end
     end
   end
