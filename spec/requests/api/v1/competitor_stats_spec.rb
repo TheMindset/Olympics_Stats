9445d94afcd7a26ffde3f308c405b4c4d4a44e09
TheMindset <mulezig2014@outlook.fr>
2019-12-27 04:43:09 +0100

    Add Competitor stats spec

---
+++ spec/requests/api/v1/competitor_stats_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Competitor Stats', type: :request do
+  before :each do
+    comp1 = create(:Competitor, sex: 'F', age: 18, height: 159, weight: 61)
+    comp2 = create(:Competitor, sex: 'M', age: 35, height: 181, weight: 75)
+    comp3 = create(:Competitor, sex: 'F', age: 27, height: 162, weight: 65)
+    comp4 = create(:Competitor, sex: 'M', age: 18, height: 174, weight: 69)
+    comp5 = create(:Competitor, sex: 'F', age: 16, height: 159, weight: 57)
+    comp6 = create(:Competitor, sex: 'F', age: 26, height: 174, weight: 61)
+    comp7 = create(:Competitor, sex: 'F', age: 31, height: 179, weight: 63)
+  end
+
+  it 'returns stats on all Competitors' do
+    get '/api/v1/competitor_stats'
+
+    expect(response).to be_successful
+
+    competitor_stats = JSON.parse(response.boby, symbolize_names: true)[:data]
+    competitor_average_weight = competitor_stats[:average_weight]
+    competitor_average_height = competitor_stats[:average_height]
+
+    expect(competitor_stats[:total_competitors]).to eq(7)
+    expect(competitor_stats[:average_age]).to eq(25)
+
+    expect(competitor_average_weight[:units]).to eq('kg')
+    expect(competitor_average_weight[:female_average]).to eq(24)
+    expect(competitor_average_weight[:male_average]).to eq(27)
+
+    expect(competitor_average_height[:units]).to eq('cm')
+    expect(competitor_average_height[:female_average]).to eq(62)
+    expect(competitor_average_height[:male_average]).to eq(72)
+  end
+end
