05b07bbee8ade1d424738fb3976ca4ebbd5c9273
TheMindset <mulezig2014@outlook.fr>
2019-12-27 08:40:35 +0100

    Refactor Events endpoint spec

--- spec/requests/api/v1/events_endpoint_spec.rb
+++ spec/requests/api/v1/events_endpoint_spec.rb
@@ -4,16 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'Events endpoint', type: :request do
   it "returns the events associate of each Sport" do
-    sport1, sport2, sport3, sport4 = create(:sport)
+    sport1, sport2, sport3, sport4 = create_list(:sport, 4)
 
-    create(:event, sport1)
-    create(:event, sport1)
-    create(:event, sport1)
-    create(:event, sport2)
-    create(:event, sport2)
-    create(:event, sport4)
-    create(:event, sport3)
-    create(:event, sport3)
+    create(:event, sport: sport1)
+    create(:event, sport: sport1)
+    create(:event, sport: sport1)
+    create(:event, sport: sport2)
+    create(:event, sport: sport2)
+    create(:event, sport: sport4)
+    create(:event, sport: sport4)
+    create(:event, sport: sport3)
 
     get '/api/v1/events'
 
@@ -22,8 +22,8 @@ RSpec.describe 'Events endpoint', type: :request do
     events_by_sport = JSON.parse(response.body, symbolize_names: true)[:data]
     last_sport_events = events_by_sport.last[:attributes]
 
-    expect(events_by_sport).to have_key(:name)
-    expect(last_sport_events).to be_an(Array)
+    expect(events_by_sport.count).to eq(4)
+    expect(last_sport_events[:events]).to be_an(Array)
     expect(last_sport_events[:events].count).to eq(2)
   end
 end
