5402c957f28d5724a5e415a1f490fde29303192a
TheMindset <mulezig2014@outlook.fr>
2019-12-27 11:28:29 +0100

    Add Medalists routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get '/competitors', to: 'competitors#index'
       get '/competitors_stats', to: 'competitors_stats#index'
       get '/events', to: 'events#index'
+      get '/medalists', to: 'medalists#index'
     end
   end
 end
