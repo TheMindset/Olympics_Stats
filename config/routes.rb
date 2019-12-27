c7379335b43942b44e846be6f745c86263d0faff
TheMindset <mulezig2014@outlook.fr>
2019-12-27 07:02:44 +0100

    Add route for /competitors_stats

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/competitors', to: 'competitors#index'
+      get '/competitors_stats', to: 'competitors_stats#index'
     end
   end
 end
