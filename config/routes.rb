8f515060901a7d9aa6365cc6872474e07144983e
TheMindset <mulezig2014@outlook.fr>
2019-12-27 08:39:02 +0100

    Add  routes for Events endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/competitors', to: 'competitors#index'
       get '/competitors_stats', to: 'competitors_stats#index'
+      get '/events', to: 'events#index'
     end
   end
 end
