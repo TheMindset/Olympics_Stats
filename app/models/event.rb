915677d32c459c272277fafe3956dc1f37feecd4
TheMindset <mulezig2014@outlook.fr>
2019-12-27 22:58:28 +0100

    Add logic for retreive event_participation for each team

--- app/models/event.rb
+++ app/models/event.rb
@@ -25,4 +25,11 @@ class Event < ApplicationRecord
   belongs_to :sport
   has_many :competitor_events
   has_many :competitors, through: :competitor_events
+
+  def self.team_events(team_id)
+    select("events.*")
+      .joins(competitors: :team)
+      .where(teams: { id: team_id })
+      .uniq.pluck(:name)
+  end
 end
